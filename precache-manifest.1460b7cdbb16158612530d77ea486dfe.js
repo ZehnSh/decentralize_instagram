78aeb24b6f44e2e482d775d4e68081a042db29b6
unknown <jenith281998@gmail.com>
2021-11-14 12:46:18 +0530

    Updates

---
+++ precache-manifest.1460b7cdbb16158612530d77ea486dfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0e6cdcfcee58fbd6ff3",
+    "url": "/decentralize_instagram/static/css/main.b03da31e.chunk.css"
+  },
+  {
+    "revision": "a0e6cdcfcee58fbd6ff3",
+    "url": "/decentralize_instagram/static/js/main.a0e6cdcf.chunk.js"
+  },
+  {
+    "revision": "37f1d9199471a65109d1",
+    "url": "/decentralize_instagram/static/css/1.22ca4ef4.chunk.css"
+  },
+  {
+    "revision": "37f1d9199471a65109d1",
+    "url": "/decentralize_instagram/static/js/1.37f1d919.chunk.js"
+  },
+  {
+    "revision": "a35af5a98009b67bd0f7",
+    "url": "/decentralize_instagram/static/js/runtime~main.a35af5a9.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/decentralize_instagram/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "ec750536c9b437bf1fda030467a25539",
+    "url": "/decentralize_instagram/index.html"
+  }
+];
